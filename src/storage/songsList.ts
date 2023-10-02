861df7a71ab5adccfc961c9f91fa117353ce96a6
Artem Lebedev <72556276+rulila52@users.noreply.github.com>
2023-10-02 14:31:05 +0300

    feat: добавить гимн Атлетико

--- src/storage/songsList.ts
+++ src/storage/songsList.ts
@@ -6,12 +6,14 @@ import movingwayupMp3 from "../assets/sounds/songs/bensound-movingwayup.mp3";
 import raindropsMp3 from "../assets/sounds/songs/bensound-raindrops.mp3";
 import thedeclineofmankindMp3 from "../assets/sounds/songs/bensound-thedeclineofmankind.mp3";
 import voyageverslenordMp3 from "../assets/sounds/songs/bensound-voyageverslenord.mp3";
+import atletiMp3 from "../assets/sounds/songs/Gimn_-_FK_Atletiko_Madrid_(Zvyki.com).mp3";
 import leagueCover from "../assets/images/covers/league_risian.jpg";
 import infinityCycleCover from "../assets/images/covers/infinitcycle_spearfisher.jpg";
 import metamorphosisCover from "../assets/images/covers/metamorphosis_laneking.jpg";
 import movingwayupCover from "../assets/images/covers/movingwayup_marcus.jpg";
 import raindropsCover from "../assets/images/covers/raindrops_veaced.jpg";
 import thedeclineofmankindCover from "../assets/images/covers/thedeclineofmankind_gefilterfish.jpg";
+import atletiCover from "../assets/images/covers/atleti-square.jpg";
 
 export const songsList: SongData[] = [
     {
@@ -49,6 +51,11 @@ export const songsList: SongData[] = [
         title: "The Decline Of Man Kind",
         artist: "Ge Filter Fish",
         cover: thedeclineofmankindCover,
+    },
+    {
+        audioFile: atletiMp3,
+        artist: "Atletico de Madrid - гимн",
+        cover: atletiCover,
     },
     {
         audioFile: voyageverslenordMp3,
